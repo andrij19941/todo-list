86fee9423e0e2fbf693af2b3cf9c34fabaa8f1dc
andrij19941 <106135455+andrij19941@users.noreply.github.com>
2023-09-01 00:40:24 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoItem from './components/TodoItem';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState([]);
+	const addTodo = (text) => {
+		let id = 1;
+		if (todos.length > 0) {
+			id = todos[0].id + 1;
+		}
+		let todo = { id: id, text: text, completed: false };
+		let newTodos = [todo, ...todos];
+		console.log(newTodos);
+		setTodos(newTodos);
+	};
+	return (
+		<div className="todo-app">
+			<h1>Todo List</h1>
+			<TodoForm addTodo={addTodo} />
+			{todos.map((todo) => {
+				return <TodoItem key={todo.id} todo={todo} />;
+			})}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { RiCloseCircleLine } from 'react-icons/ri';
+export default function TodoItem(props) {
+	return (
+		<div className="todo-row">
+			{props.todo.text}
+			<div className="iconsContainer">
+				<RiCloseCircleLine className="icon" />
+			</div>
+		</div>
+	);
+}
