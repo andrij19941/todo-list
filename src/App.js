3d4b2df5818796b67d7c597c2bd15960b45b851c
andrij19941 <106135455+andrij19941@users.noreply.github.com>
2023-09-02 20:38:59 +0300

    3

--- src/App.js
+++ src/App.js
@@ -12,19 +12,28 @@ function App() {
 		}
 		let todo = { id: id, text: text, completed: false };
 		let newTodos = [todo, ...todos];
-		console.log(newTodos);
 		setTodos(newTodos);
 	};
 	const removeTodo = (id) => {
 		let updatedTodos = [...todos].filter((todo) => todo.id !== id);
 		setTodos(updatedTodos);
 	};
+	const completeTodo = (id) => {
+		let updatedTodos = todos.map((todo) => {
+			if (todo.id === id) {
+				todo.completed = !todo.completed;
+			}
+			return todo;
+		});
+		setTodos(updatedTodos);
+	};
 	return (
 		<div className="todo-app">
 			<h1>Todo List</h1>
 			<TodoForm addTodo={addTodo} />
+			<hr className="seperator"></hr>
 			{todos.map((todo) => {
-				return <TodoItem removeTodo={removeTodo} key={todo.id} todo={todo} />;
+				return <TodoItem completeTodo={completeTodo} removeTodo={removeTodo} key={todo.id} todo={todo} />;
 			})}
 		</div>
 	);
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,13 +3,13 @@ import { RiCloseCircleLine } from 'react-icons/ri';
 import { BiCheckCircle } from 'react-icons/bi';
 
 export default function TodoItem(props) {
-	const { todo, removeTodo } = props;
+	const { todo, removeTodo, completeTodo } = props;
 	return (
-		<div className="todo-row">
+		<div className={todo.completed ? 'todo-row complete' : 'todo-row'}>
 			{todo.text}
 			<div className="iconsContainer">
-				<RiCloseCircleLine  onClick={() => removeTodo(props.todo.id)} />
-				<BiCheckCircle onClick={}/>
+				<RiCloseCircleLine onClick={() => removeTodo(props.todo.id)} />
+				<BiCheckCircle onClick={() => completeTodo(todo.id)} />
 			</div>
 		</div>
 	);
